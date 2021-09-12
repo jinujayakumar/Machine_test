d3765ceb35fae71a9def6fb5f378375d404c2092
Jinu J <jinu.j@quest-global.com>
2021-09-13 01:17:22 +0530

    Search api call requested.
    Signed-off-by: Jinu J <jinu.j@quest-global.com>

--- app/src/main/java/com/example/kolomachinetest/ui/filter/FilterAdapter.kt
+++ app/src/main/java/com/example/kolomachinetest/ui/filter/FilterAdapter.kt
@@ -3,6 +3,8 @@ package com.example.kolomachinetest.ui.filter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.kolomachinetest.R
 import com.example.kolomachinetest.api.PaginationCallback
 import com.example.kolomachinetest.data.ApiResponse
@@ -13,11 +15,12 @@ class FilterAdapter(list: MutableList<Result>, callback: PaginationCallback) :
     PaginationAdapter<FilterAdapter.FilterViewHolder, Result>(list, callback) {
 
     class FilterViewHolder(view: View) : PaginationViewHolder(view) {
-
+        val imageView : ImageView = view.findViewById<ImageView>(R.id.imageView)
+        val textView : TextView = view.findViewById<TextView>(R.id.textView)
     }
 
     override fun onBindViewHolder(holder: FilterViewHolder, pos: Int, model: Result) {
-
+        holder.textView.text = model.title
     }
 
     override fun onCreateItemViewHolder(parent: ViewGroup, viewType: Int): FilterViewHolder {
